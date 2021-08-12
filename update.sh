3d6813a8ec20bef9230cf6dd3cfa994f28aa0ac5
SeongJae Park <sj38.park@gmail.com>
2021-08-12 07:23:47 +0200

    update: Do not remove README file
    README file is not from broken-out.tar.gz but from this repository.
    Don't remove it.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 bindir=$(dirname "$0")
 cd $bindir || exit 1
 
-rm -fr broken-out.tar.gz broken-out/ series README .DATE*
+rm -fr broken-out.tar.gz broken-out/ series .DATE*
 
 wget https://www.ozlabs.org/~akpm/mmotm/broken-out.tar.gz
 tar -xf broken-out.tar.gz
