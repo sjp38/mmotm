b91ab081fed8d81c521fa07bba38ec501a3e52d3
SeongJae Park <sj38.park@gmail.com>
2021-08-12 07:29:21 +0200

    update: Fetch mmotm-readme.txt
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ cd $bindir || exit 1
 
 rm -fr broken-out.tar.gz broken-out/ series .DATE*
 
+wget https://www.ozlabs.org/~akpm/mmotm/mmotm-readme.txt
 wget https://www.ozlabs.org/~akpm/mmotm/broken-out.tar.gz
 tar -xf broken-out.tar.gz
 rm -f broken-out.tar.gz
