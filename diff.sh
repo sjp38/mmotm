869ba33887dfd25cf66b1f9536ad11b1b103576b
SeongJae Park <sj38.park@gmail.com>
2021-08-13 07:22:50 +0200

    diff: Improve the format
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- diff.sh
+++ diff.sh
@@ -10,10 +10,29 @@ from=$1
 to=$2
 
 diff_output=$(git diff "$from".."$to" --name-status | grep broken-out/)
-echo "$diff_output"
+
+dropped=$(echo "$diff_output" | grep '^D' | grep 'broken-out')
+added=$(echo "$diff_output" | grep '^A' | grep 'broken-out')
+modified=$(echo "$diff_output" | grep '^M' | grep 'broken-out')
+
+echo "Dropped"
+echo "----"
+echo
+echo "$dropped"
+echo
+echo "Added"
+echo "-----"
+echo
+echo "$added"
+echo
+echo "Modified"
+echo "--------"
+echo
+echo "$modified"
+echo
 
 dropped=$(echo "$diff_output" | grep '^D' | grep 'broken-out' | wc -l)
 added=$(echo "$diff_output" | grep '^A' | grep 'broken-out' | wc -l)
 modified=$(echo "$diff_output" | grep '^M' | grep 'broken-out' | wc -l)
 
-echo "$dropped drop, $added add, $modified modified"
+echo "summary: $dropped drop, $added add, $modified modified"
