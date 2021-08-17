ac82c3e37fe725fbc784bc34c6369b2c928e3e03
SeongJae Park <sj38.park@gmail.com>
2021-08-17 07:07:04 +0200

    update: Remove mmotm-readme.txt before fetching new one
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- update.sh
+++ update.sh
@@ -3,9 +3,10 @@
 bindir=$(dirname "$0")
 cd $bindir || exit 1
 
-rm -fr broken-out.tar.gz broken-out/ series .DATE*
-
+rm -f mmotm-readme.txt
 wget https://www.ozlabs.org/~akpm/mmotm/mmotm-readme.txt
+
+rm -fr broken-out.tar.gz broken-out/ series .DATE*
 wget https://www.ozlabs.org/~akpm/mmotm/broken-out.tar.gz
 tar -xf broken-out.tar.gz
 rm -f broken-out.tar.gz
