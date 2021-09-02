9cebb42878186559642d0f441ed14e6ae87ddb1b
SeongJae Park <sj38.park@gmail.com>
2021-09-02 07:38:01 +0200

    Add a script for diffs stat history
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ diffs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+commits=$(git log --pretty='%h' -- broken-out/)
+for commit in $commits
+do
+	diff_output=$(git diff "$commit^".."$commit" --name-status \
+		-- broken-out/)
+	dropped=$(echo "$diff_output" | grep '^D' | wc -l)
+	added=$(echo "$diff_output" | grep '^A' | wc -l)
+	modified=$(echo "$diff_output" | grep '^M' | wc -l)
+	date=$(git show "$commit" --pretty='%ad' --quiet)
+
+	echo "$date: $added adds, $modified modifications, $dropped drops"
+done
