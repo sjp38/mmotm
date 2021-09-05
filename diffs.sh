2444fbe415d537bc23bbd9ac1f9c5dcb3d38bf7c
SeongJae Park <sj38.park@gmail.com>
2021-09-05 12:41:02 +0200

    diffs: Fix shellcheck errors
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- diffs.sh
+++ diffs.sh
@@ -3,11 +3,11 @@
 commits=$(git log --pretty='%h' -- broken-out/)
 for commit in $commits
 do
-	diff_output=$(git diff "$commit^".."$commit" --name-status \
+	diff_output=$(git diff "$commit^..$commit" --name-status \
 		-- broken-out/)
-	dropped=$(echo "$diff_output" | grep '^D' | wc -l)
-	added=$(echo "$diff_output" | grep '^A' | wc -l)
-	modified=$(echo "$diff_output" | grep '^M' | wc -l)
+	dropped=$(echo "$diff_output" | grep -c '^D')
+	added=$(echo "$diff_output" | grep -c '^A')
+	modified=$(echo "$diff_output" | grep -c '^M')
 	date=$(git show "$commit:.DATE" | head -n 1)
 	base=$(git show "$commit:.DATE" | tail -n 1)
 
