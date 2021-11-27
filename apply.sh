4791232109a2e3cf5a3f5674b88e2837134b52f5
SeongJae Park <sj38.park@gmail.com>
2021-11-27 09:45:12 +0100

    apply: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- apply.sh
+++ apply.sh
@@ -8,10 +8,10 @@ fi
 
 linux_repo=$1
 
-bindir=$(dirname $(realpath $0))
+bindir=$(dirname "$(realpath "$0")")
 
 date="$bindir/.DATE"
-base_version="v$(cat "$date" | tail -n 1)"
+base_version="v$(tail -n 1 "$date")"
 
 echo "base version: $base_version"
 if ! git -C "$linux_repo" checkout "$base_version"
@@ -23,11 +23,15 @@ fi
 series="$bindir/series"
 patchesdir="$bindir/broken-out"
 
-for patch in $(grep "^[^#]" $series)
+while read -r patch
 do
+	if echo "$patch" | grep "^#" -q
+	then
+		continue
+	fi
 	if ! git -C "$linux_repo" am "$patchesdir/$patch"
 	then
-		echo "failed applying $patch"
+		echo "Failed applying $patch"
 		break
 	fi
-done
+done < "$series"
